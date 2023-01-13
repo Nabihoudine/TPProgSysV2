29ce25a43c7b6cc5c22845948b5fba0d3c348e31
nabiamad29 <nabi2001@outlook.fr>
2023-01-13 13:55:45 +0100

    //06/01/2022 question 6

--- fonctions.c
+++ fonctions.c
@@ -14,37 +14,65 @@ void display_prompt(void)
 
 }
 
-void commande(void)
-{
-    int nbCharRead;
+void commande(void) {
+    struct timespec start, end;
+    int nbCharRead,nbCharRead2,nbCharRead3;
+    long t;
     int status;
-    char buf[MaxSize];
-    nbCharRead=read(1,buf,MaxSize);
-    buf[nbCharRead-1]='\0';
-    pid_t ret=fork();
+    char buf[MaxSize]={0};
+    const char *arg[MaxSize]={0};
+    char *token;
+    clock_gettime(CLOCK_REALTIME,&start);
+    //nbCharRead = read(1, buf, MaxSize);
+    nbCharRead2 = read(1, arg, MaxSize);
+    //buf[nbCharRead - 1] = '\0';
+    arg[nbCharRead2 -1] = '\0';
+    pid_t ret = fork();
     fork();
-    if(fork()==0)
-    {
-        execlp(buf,buf,NULL);
-        sprintf(out_buf,Command_dont_exist,buf);
-        write(1,out_buf,strlen(out_buf));
-        exit(EXIT_FAILURE);
-
-    }
-
-        wait(&status);
-        display_prompt();
-
-        if(buf=="exit")
+    if (fork() == 0) {
+        token = strtok(buf, arg);
+        while (token != NULL)
         {
-            write(1,msg_exit,sizeof(exit));
+            //execlp(buf, buf, NULL);
+            token =strtok(NULL,arg);
+            execvp(arg[0], arg);
+            exit(EXIT_FAILURE);
+            if (WIFEXITED(status)) {
+                clock_gettime(CLOCK_REALTIME, &end);
+                t = (end.tv_sec - start.tv_sec);
+                sprintf(buf, prompt_exit, WEXITSTATUS(status), t);
+                write(1, buf, strlen(buf));
+            } else {
+                clock_gettime(CLOCK_REALTIME, &end);
+                t = (end.tv_sec - start.tv_sec);
+                sprintf(buf, prompt_sign, WSTOPSIG(status), t);
+                write(1, buf, strlen(buf));
+            }
+            // sprintf(out_buf, Command_dont_exist, buf);
+            //write(1, out_buf, strlen(out_buf));
+            //exit(EXIT_FAILURE);
+        }
+    }
+    wait(&status);
+    display_prompt();
+
+
+
+
+
+    if (strcmp(buf,"exit")==0)
+        {
+            write(1,msg_exit,strlen(msg_exit));
             exit(EXIT_SUCCESS);
 
 
         }
-    }
+
+}
+
     //read(1,message,sizeof(message));
     //fork();
     //write(1,"Today is what happened to yesterday",sizeof("Today is what happened to yesterday")-1);
+
 
 
--- fonctions.h
+++ fonctions.h
@@ -3,15 +3,19 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <string.h>
+#include <time.h>
 
 #define welcome_msg "Bienvenue dans le shell ENSEA\nPour quitter,taper 'exit'\n"
-#define Command_dont_exist "commade doesnt exist\n"
+#define Command_dont_exist "commande doesnt exist\n"
 #define msg_exit "Bye Bye\n"
 #define prompt "enseash %"
-#define cmd "ls"
+#define prompt_exit "[exit :%d |%llu ms]\n"
+#define prompt_sign "[sign :%d |%llu ms]\n"
+
 #define MaxSize 512
 #define out_buf 128
 
 void display_msg(void);
 void display_prompt(void);
 void commande(void);
+
